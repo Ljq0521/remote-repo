a83f7d42b8d4e2585fc2603952eb1d9e7d58eb71
Jinqi Li <1113784294@qq.com>
2025-05-03 18:07:06 +0200

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        for (int integer= 0; integer < args.length; integer++) {
+            System.out.println(args[integer]);
+        }
+    }
+}
